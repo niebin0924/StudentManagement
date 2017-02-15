e1a5ee1bbc6c4838703127efa11333e44f690b51
niebin0924 <804186194@qq.com>
2017-02-15 14:33:45 +0800

    Initial Commit

---
+++ StudentManagement/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StudentManagement
+//
+//  Created by Kitty on 17/2/15.
+//  Copyright © 2017年 Kitty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
